d335cebf29a6404983b3c33cb582e7a539286b94
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-11-15 20:20:36 +0545

    collision game added

--- Collision/script.js
+++ Collision/script.js
@@ -4,13 +4,13 @@ canvas.width="600";
 canvas.height="600";
 let color = "red";
 
-
+let counter =1;
 
 class Object{
     constructor(){
         this.position={
             x:Math.random()* 600,
-            y:Math.random()*600
+            y:200
         }
         this.size={
             width:50,
@@ -18,7 +18,7 @@ class Object{
         }
         this.velocity={
             x:Math.random()*(3 - 1) + -1,
-            y:3
+            y:Math.random()*(5 - 1) + -1
         }
     }
     // its draw an rectangle
@@ -31,29 +31,35 @@ class Object{
         c.stroke();
 
     }
+    
     // it moves the rectangle
     move(){
         // this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
+        if(counter < 3){
+            this.position.y += this.velocity.y;
+        }
     }
     // checks X axis collision
     borderX_Collision(){
         if(this.position.x + this.size.width >= canvas.width){
             this.velocity.x=3*(-1)
             color="blue"
-            console.log(this.velocity.x)
+
         }
         else if( this.position.x <0){
             this.velocity.x=3*(1)
             color ="green"
-        console.log(this.position.x);
+
         } 
     }
     //checks Y axis collision
     borderY_Collision(){
         if(this.position.y + this.size.height>=canvas.height){
-            this.velocity.y =3*(-1);
-            color ="black"
+            // this.velocity.y =3*(-1);
+            // color ="black"
+            this.position.y = canvas.height- this.size.height;
+            counter++;
+            console.log(counter);
         }
         else if(this.position.y <=0){
             this.velocity.y =3*(1);
@@ -61,37 +67,53 @@ class Object{
         }
 
     }
-    removeBox(){
-        if(this.position.y >= 500){
-            this.position.y =600;
-        }
-    }
+    
     //it calls draw and move
     update(){
         this.draw();
         this.move();
         this.borderX_Collision();
         this.borderY_Collision();
-        this.removeBox();
+       
 
     }
 
 }
 // makes new object
 
-let box=[]
-for(let i =0;i<=5;i++){
+let box=[];
+
+for(let i =0;i<=10;i++){
     let obj = new Object();
     box.push(obj);
 }
+let x=0;
+let y=0;
+canvas.addEventListener("mousemove",(event) =>{
+    x=event.clientX;
+    y=event.clientY;
+
+   
+
+});
 
 
 function animate(){
     c.clearRect(0,0,canvas.width,canvas.height);
-    for(let i=0;i<=5;i++){
+    c.beginPath();
+    c.fillStyle="black"
+    c.fillRect(x,y,10,10);
+    for(let i=0;i<=10;i++){
         box[i].update();
+        if(x+10>box[i].position.x && box[i].position.x + box[i].size.width >x &&
+            y+10>box[i].position.y && box[i].position.y + box[i].size.height >y ) {
+                box[i].position.y = -100;
+            }
     }
+   
 
+    
+    
     requestAnimationFrame(animate);
 }
 animate();
